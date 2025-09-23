dc8ac85a1e1a9af68c45c6f553155c13f27aaedd
Roshith Sandeepa <rosandeepasamith@gmail.com>
2025-09-24 01:04:27 +0530

    updated main_api.py

--- backend/src/api/main_api.py
+++ backend/src/api/main_api.py
@@ -7,3 +7,15 @@ from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+@app.post("/register/", response_model=schemas.UserInDB)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    hashed_password = security.get_password_hash(user.password)
+    new_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
