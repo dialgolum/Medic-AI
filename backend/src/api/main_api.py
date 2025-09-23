378bcaceda1c05f3bacab7a73f52c8040c9f115b
Roshith Sandeepa <rosandeepasamith@gmail.com>
2025-09-24 01:07:16 +0530

    Done main_api.py

--- backend/src/api/main_api.py
+++ backend/src/api/main_api.py
@@ -19,3 +19,15 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.post("/token", response_model=schemas.Token)
+def login_for_access_token(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == user_data.username).first()
+    if not user or not security.verify_password(user_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = security.create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
